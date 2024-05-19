f34082f1233d4e514dd4b07aa1a3b10a7a372ef4
esilberberg <ericsil218@gmail.com>
2024-05-18 22:26:16 -0400

    components, pages, orgi

---
+++ components/primary-footer.js
@@ -0,0 +1,15 @@
+const primaryFooter = document.getElementById('primary-footer')
+
+const footerContent = `
+<div class="primary-footer-content">
+<img src="media/khhp-logo-white.png" alt="Kentuck Hispanic Heritage Project">
+<div class="footer-socials">
+    <a href="https://www.instagram.com/kyhispanicheritageproject/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-square-instagram"></i></a>
+    <a  href="https://www.facebook.com/KYHispanicHeritageProject" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-square-facebook"></i></a>
+</div>
+<p>info@khhp.org</p>
+<p>Copyright &#169; 2023-2024 | Site by <a href="https://github.com/esilberberg" target="_blank" rel="noopener noreferrer">esilberberg</a></p>
+</div>
+`
+
+primaryFooter.innerHTML = footerContent;
